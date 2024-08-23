9991ea5d98dabff236fc5ffc5fd1f5f7fda7040a
Niraj Gupta <neerajfullstack@gmail.com>
2024-08-24 01:34:55 +0530

    wrap up

--- script/index.js
+++ script/index.js
@@ -6,7 +6,6 @@ $('.show-filters').on('click', function(){
   3. Otherwise, if the nav if collapsed, expand it
   4. Change the text to say hide filters
   */
-
   if($('.filters-list').is(':visible')) {
     $('.filters-list').slideUp();
     $('.show-filters').text('Show Filters')
@@ -15,5 +14,21 @@ $('.show-filters').on('click', function(){
     $('.show-filters').text('Hide Filters')
   }
 
+  return false;
+
+})
+
+
+$('.filters-list a').on('click', function(){
+    var filter = $(this).attr('data-filter')
+  // 1. Hide all the products
+  // 2. Show the products width a particular filter
+    $('.product').hide();
+    $(filter).show();
+    $('.filters-list a').removeClass('selected');
+    $(this).addClass('selected');
+    
+  
+  return false;
 
 })
